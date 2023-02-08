2057335a6f8c8a5f6a37a2874e6a190114f347fb
Gal Rosenthal <gal.rosenthal@taboola.com>
2023-02-08 12:44:36 +0200

    set empty popup on isLogin

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import "@pages/popup/Popup.css";
 import BasicTabs from "@pages/components/BasicTabs";
 import AppBarMenu from "@pages/components/AppBarMenu";
 
 const Popup = () => {
+    const [islogin , setLogin  ] = useState(false)
+
+
   return (
-    <div >
-      <AppBarMenu/>
-      <BasicTabs/>
+    <div>
+        <AppBarMenu islogin={islogin}/>
+
+        {islogin &&  <>  <BasicTabs/> </> }
     </div>
   );
 };
