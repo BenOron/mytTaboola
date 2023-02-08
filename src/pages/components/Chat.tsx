90562db683bfe8492faa50c7063a3b5ad9a6e49e
Ben Oron <Ben.o@taboola.com>
2023-02-08 22:28:10 +0200

    be

---
+++ src/pages/components/Chat.tsx
@@ -0,0 +1,10 @@
+import { ChatFeed, Message } from 'quannt-react-chat-ui'
+import React, {useState, useEffect, useContext} from "react";
+
+// Your code stuff...
+const Chat = () => {
+    return (<></>)
+}
+
+
+export default Chat
--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -2,7 +2,7 @@ import { useEffect,useState } from "react";
 
 export default function App() {
   const [extractData,setExtractData] = useState({})
-  const [dom,setDom] = useState({title:'p'})
+  const [dom,setDom] = useState({})
   useEffect(() => {
     console.log(extractData)
   },[extractData]);
@@ -20,7 +20,10 @@ export default function App() {
     const description = doc.querySelectorAll("P");
     const images = doc.querySelectorAll("img");
 
-
+    setDom((prevState) => ({
+      ...prevState,
+      title: title[0].className,
+    }));
 
     setExtractData( {
       title: title.length > 0 ? title[0].textContent : "",
@@ -32,6 +35,38 @@ export default function App() {
     });
   }
 
+  const setTitle = (suggeted)=>{
+    let titleElm = document.getElementsByClassName(dom.title)[0]
+    titleElm.textContent = suggeted
+  }
 
-  return <div className="content-view">content view11</div>;
+  const FooterElm ='<div className="footer-mytaboola-title" style="position: relative;' +
+      '    width: 100%;\n' +
+      '    height: 30px;\n' +
+      '    vertical-align: top;\n' +
+      '    background-color: #9aafe142;\n' +
+      '    color: white;\n' +
+      '    text-align: center;"\n' +
+          '<div style="vertical-align: inherit;float: right;" > ' +
+      '<div style="font-size: 20px;color: black;float: left;padding: 7px;">' +
+              'MyTaboola' +
+            '</div>' +
+          '<div style="vertical-align: inherit;float: right;padding: 5px;cursor: pointer" >' +
+            '<div style="vertical-align: inherit;"><a onclick=(this.parentElement.style.display = "none";) style="font-size: 25px; vertical-align: inherit;">&#8249;</a>' +
+            ' <a style="font-size: 25px; vertical-align: inherit;">&#8250;</a></div>' +
+          '</div>' +
+      '</div>'
+
+
+  useEffect(() => {
+    if(dom?.title){
+      let titleElm = document.getElementsByClassName(dom.title)[0]
+      let child = document.createElement('div')
+      child.innerHTML = FooterElm;
+      titleElm.appendChild(child)
+    }
+  },[dom]);
+  return <div className="content-view">
+
+  </div>;
 }
