0485d1771b94531d071a0cfc566e43133083ecd7
Gal Rosenthal <gal.rosenthal@taboola.com>
2023-02-08 14:19:28 +0200

    login page ready, need to style

--- src/pages/components/LoginPage.tsx
+++ src/pages/components/LoginPage.tsx
@@ -2,6 +2,14 @@ import React, {useState, useEffect} from "react";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import axios from 'axios';
+import Box from "@mui/material/Box";
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Switch from '@mui/material/Switch';
+import {Stack} from "@mui/material";
 
 
 const LoginPage = () => {
@@ -11,15 +19,28 @@ const LoginPage = () => {
         const url = "https://www.google.com";
         axios.get(url).then((res) => {
             console.log(res.status);
+            if (res.status == 201){
+                setLogin(true);
+            }
         });
     }
 
 
     return (
-        <div>
-            <TextField id="standard-basic" label="Standard" variant="standard" />
-            <Button onClick={getUrl}> Login </Button>
-        </div>
+
+        <Box
+             sx={{
+                 p: 10,
+             }}
+        >
+            <Stack>
+                <TextField id="standard-basic" label="Email" variant="standard" />
+                <TextField id="standard-basic" label="Password" type="password" variant="standard" />
+                <Button onClick={getUrl}> Login </Button>
+            </Stack>
+        </Box>
+
+
     );
 };
 
