b4864e651a314f44c3b9e3031e4e5cc7575d41bf
Ben Oron <Ben.o@taboola.com>
2023-02-08 19:47:13 +0200

    test

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -2,10 +2,14 @@ import { useEffect,useState } from "react";
 
 export default function App() {
   const [extractData,setExtractData] = useState({})
+  const [dom,setDom] = useState({})
+  useEffect(() => {
+    console.log(extractData)
+  },[extractData]);
 
 
   useEffect(() => {
-    setExtractData(extractDataFromHtml( document.getElementsByTagName('article')[0].innerHTML))
+    extractDataFromHtml( document.getElementsByTagName('article')[0].innerHTML)
   },[]);
 
   const extractDataFromHtml = (html) =>{
@@ -16,14 +20,16 @@ export default function App() {
     const description = doc.querySelectorAll("P");
     const images = doc.querySelectorAll("img");
 
-    return {
+
+
+    setExtractData( {
       title: title.length > 0 ? title[0].textContent : "",
       description: description.length > 0 ? [].map.call(doc.querySelectorAll("p"), function(el) {
         return el?.innerHTML;
       }) : "",
       images: Array.from(images).map(img => img.src)
 
-    };
+    });
   }
 
 
